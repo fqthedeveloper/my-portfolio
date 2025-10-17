33c544ec1033b5761930233373c90cee0bd71deb
Faizan Qureshi <74370508+fqthedeveloper@users.noreply.github.com>
2025-10-17 12:10:36 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Container } from "react-bootstrap";
 import Particle from '../components/Particle';
 import ContactForm from '../components/Contact/Contact';
 import Social from '../components/Contact/Social';
 
 const Contact = () => {
+    useEffect(() => {
+    document.title = 'Contact Page';
+  });
+  
   return (
     <Container style={{padding: '60px'}}>
       <Particle />
